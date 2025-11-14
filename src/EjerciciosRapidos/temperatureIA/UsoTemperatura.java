3908a4b5e237e61bffa89abfb9d3b3ffd4e5b762
Francisco José Lamela López <skonner87@gmail.com>
2025-11-14 13:27:34 +0100

    Temperature Controller

--- src/EjerciciosRapidos/temperatureIA/UsoTemperatura.java
+++ src/EjerciciosRapidos/temperatureIA/UsoTemperatura.java
@@ -1,7 +1,12 @@
 package EjerciciosRapidos.temperatureIA;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,48 +20,56 @@ public class UsoTemperatura {
 }
 
 class Termostato implements Regulable {
-	int temperaturaActual= 20;
+	private int temperaturaActual= 20;
 	
 	@Override
 	public void subirTemperatura() {
 		// TODO Auto-generated method stub
 		if (temperaturaActual < Regulable.TEMPERATURA_MAXIMA) temperaturaActual++;
+		else System.out.println("No puedes subir más de 30ºC. ¿A caso quieres derretirte?");
 	}
 
 	@Override
 	public void bajarTemperatura() {
 		// TODO Auto-generated method stub
 		if (temperaturaActual > Regulable.TEMPERATURA_MINIMA) temperaturaActual--;
+		else System.out.println("No puedes bajar de 10ºC. Te entiendo, el calor es insoportable.");
 	}
 	
 	public int getTemp() {
 		return temperaturaActual;
 	}
-	
 }
 
 class MiFrame extends JFrame {
+	Termostato termostato= new Termostato();
+	
 	public MiFrame() {
 		setTitle("Termostato");
 		setBounds(300,300,600,400);
-		add(new MiLamina());
+		add(new MiLamina(this.termostato));
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
 
-class MiLamina extends JPanel {
+
+class MiLamina extends JPanel implements ActionListener {
 	JButton subirTemp= new JButton("Subir Temperatura");
 	JButton bajarTemp= new JButton("Bajar Temperatura");
 	
-	Termostato t= new Termostato();
-	String aTemp= String.valueOf(t.getTemp());
-	JLabel temp= new JLabel(aTemp + "ºC");
+	private Termostato t;
+	private JLabel temp;
 	
-	public MiLamina () {
+	public MiLamina (Termostato t) {
+		this.t= t;
+		String aTemp= String.valueOf(t.getTemp());
+		temp = new JLabel(aTemp + "ºC");
 		setLayout(null);
-		subirTemp.setBounds(60, 250,200,30);
-		bajarTemp.setBounds(290, 250,200,30);
+		setFocusable(true);
+		addKeyListener(new KeyController());
+		bajarTemp.setBounds(60, 250,200,30);
+		subirTemp.setBounds(290, 250,200,30);
 		temp.setFont(new Font("SanSerif", Font.BOLD, 90));
 		temp.setBounds(200, 50, 200, 200);
 		temp.setPreferredSize(new Dimension(200,200));
@@ -65,6 +78,33 @@ class MiLamina extends JPanel {
 		add(bajarTemp);
 		add(temp);
 		
+		subirTemp.addActionListener(this);
+		bajarTemp.addActionListener(this);
+		
+		setBackColor();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == subirTemp) t.subirTemperatura();
+		else if (e.getSource() == bajarTemp) t.bajarTemperatura();
+		temp.setText(t.getTemp() + "ºC");
+		setBackColor();
+	}
+
+	class KeyController extends KeyAdapter {
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_UP) t.subirTemperatura();
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN) t.bajarTemperatura();
+			temp.setText(t.getTemp() + "ºC");
+			setBackColor();
+		}
 	}
 	
+	public void setBackColor() {
+		if (t.getTemp() > 25) setBackground(new Color(219, 79, 79));
+		else if (t.getTemp() < 15) setBackground(new Color(123, 184, 219));
+		else setBackground(new Color(222, 215, 215));
+	}
 }
